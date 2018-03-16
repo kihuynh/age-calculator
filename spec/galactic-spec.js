ab032e000ff3d92384edc925f6493fe0af801326
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-03-16 10:28:47 -0700

    in process of writing spec and research

--- spec/galactic-spec.js
+++ spec/galactic-spec.js
@@ -1,1 +1,21 @@
-// import { Galactic } from './../src/galactic.js';
+import { Galactic } from './../src/galactic.js';
+
+
+describe('Galactic', function () {
+  it('should convert a persons age into seconds', function () {
+    let age = new galacticAge();
+
+  });
+});
+
+
+
+// describe('Haiku', function() {
+//   it('takes in haiku line and returns the number of syllables', function() {
+//     let newHaiku = new Haiku("haiku");
+//     let syllableCount = 2;
+//     let haikuLine = "haiku"
+//     expect(newHaiku.countWordSyllables(haikuLine)).toEqual(syllableCount);
+//   })
+//
+// }
